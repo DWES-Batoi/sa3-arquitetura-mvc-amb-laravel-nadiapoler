2bdc5e6d663b2dc718645f92f6b0add95195e7ee
nadiapoler <nadia.poler@gmail.com>
2025-11-17 19:50:29 +0100

    Construcción completa de la estructura Laravel

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EquipController;
+
+
+// Ruta de bienvenida (GET /)
+Route::get('/', fn() => "Benvingut a la Guia d'Equips de Futbol Femení!");
+
+// Genera automáticamente varias rutas REST para 'equips'
+Route::resource('equips', EquipController::class);
