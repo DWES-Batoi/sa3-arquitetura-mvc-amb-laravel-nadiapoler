94897d6a4680670f66d39f471f1aeb8df103cb05
nadiapoler <nadia.poler@gmail.com>
2025-11-20 20:00:02 +0100

    Fase 1 completada

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EquipController;
-
+use App\Http\Controllers\EstadiController;
 
 // Ruta de bienvenida (GET /)
 Route::get('/', fn() => "Benvingut a la Guia d'Equips de Futbol Femení!");
 
 // Genera automáticamente varias rutas REST para 'equips'
 Route::resource('equips', EquipController::class);
+
+// Genera automáticamente varias rutas REST para 'estadis'
+Route::resource('estadis', EstadiController::class);
